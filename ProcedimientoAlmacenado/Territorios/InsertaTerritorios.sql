ab1bd1ee42b88d91b749bf506a9f3ff2a3d8beeb
wazowskymeso <migueljjqq@gmail.com>
2022-08-27 00:58:08 -0600

    Update InsertaTerritorios.sql

--- ProcedimientoAlmacenado/Territorios/InsertaTerritorios.sql
+++ ProcedimientoAlmacenado/Territorios/InsertaTerritorios.sql
@@ -1,7 +1,7 @@
 use northwind;
 
 DELIMITER $$
-CREATE PROCEDURE InsertaTerritorios (IN DescriTerritorio varchar(50), IN RegionID, longblob)
+CREATE PROCEDURE InsertaTerritorios (IN DescriTerritorio varchar(50), IN RegionID Int)
 BEGIN
 Select @territorioID := max(TerritoryID)+1 from Territories;
  INSERT INTO Territories (TerritoryID, TerritoryDescription, RegionID)
